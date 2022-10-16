917406875466971d59a9c40fc5f375a116cc9ca0
tsnAnh <tsnanh0812@gmail.com>
2022-10-16 22:15:55 +0700

    init

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+const PROJECT_ID = "25dc0faf706e414c840be5c220004aa3";
+
+const fs = require("fs");
+const keyData = fs.readFileSync("./p-key.txt", {
+  encoding: "utf-8",
+  flag: "r",
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${PROJECT_ID}`,
+      accounts: [keyData],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${PROJECT_ID}`,
+      accounts: [keyData],
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
